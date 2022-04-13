961604f7adb8177a36c9c160d5d5e41b50814d73
kc71486 <100687720+kc71486@users.noreply.github.com>
2022-04-13 11:01:05 +0800

    Update hw8.js

--- dist/hw8.js
+++ dist/hw8.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	document.querySelector('#ajax-form button[type="submit"]').onclick = (event) => {
 		event.preventDefault();
 		// Step 9 and step 10 code goes here
-		$.get('./step5', {
+		$.get('./helloname', {
 			fname: $('#ajax-form [name=first_name]').val(),
 			lname: $('#ajax-form [name=last_name]').val()
 		}, (data) => {
