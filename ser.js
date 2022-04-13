cdd538df996cfb7137511ce3cda594b74237a28d
kc71486 <100687720+kc71486@users.noreply.github.com>
2022-04-13 11:00:24 +0800

    Update ser.js

--- ser.js
+++ ser.js
@@ -3,11 +3,13 @@
 import express from 'express'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+import bodyParser from 'body-parser'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const app = express()
-const port = [port]
+
+const app = express();
+const port = 1264;
 
 app.listen(port, () => {
   console.log('listening on port: ' + port);
@@ -15,6 +17,9 @@ app.listen(port, () => {
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/step5', (req, res) => {
-  res.send(`Hello, ${req.query.fname} ${req.query.lname}`)
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/helloname', (req, res) => {
+  res.send(`Hello, ${req.body.fname} ${req.body.lname}`)
+});
