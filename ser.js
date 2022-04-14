ce1a093be1aefbb8950dde8c6865bc29b026de46
kc71486 <linda20030102@gmail.com>
2022-04-14 15:42:52 +0800

    solved storage issues

--- ser.js
+++ ser.js
@@ -23,30 +23,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 function saveJSON(src, obj) {
-	let tmp = JSON.stringify(obj), fs = require(fs);
-	fs.writefile(jsonpath, tmp, function (err) {
+  let tmp = JSON.stringify(obj), fs = require('fs');
+	fs.writeFile(jsonpath, tmp, function (err) {
 		if (err) {
 			console.log(err);
 		}
 	});
 }
 function loadJSON(src) {
-	let out, fs = require(fs);
-	fs.readFile(jsonpath, function (err,data) {
-		if (err) {
-			return console.log(err);
-		}
-		out = JSON.parse(data.toString());
-	});
-	return out;
+	return require("./students.json");
 }
 
 var allstu = loadJSON(jsonpath);
 
 app.get('/allstu', (req, res) => {
-	let allstu2 = loadJSON(jsonpath), stulist = "";
-	console.log("file = cache ?" + (allstu === allstu2));
-	Object.entries(allstu2).forEach(([key, value]) => {
+	let stulist = "";
+  //use cache
+  Object.entries(allstu).forEach(([key, value]) => {
 		stulist += `\"${key}\":\"${value}\"<br>`
 	})
 	res.send(stulist);
