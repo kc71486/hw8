47174e051077c73b6bc8b1b01560867e287ae7eb
kc71486 <linda20030102@gmail.com>
2022-04-14 12:04:27 +0800

    implements json read&write(experimemtal)

--- ser.js
+++ ser.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url'
 import bodyParser from 'body-parser'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const jsonpath = "students.json";
 
 const app = express();
 const port = 1268;
@@ -19,11 +20,25 @@ app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var allstu = {};
+function saveJSON(src, obj) {
+	let tmp = JSON.stringify(obj), fs = require('fs');
+	fs.writeFile(src, tmp, function(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+}
+function loadJSON(src) {
+	let tmp = require(src);
+	return JSON.parse(tmp);
+}
+
+var allstu = loadJSON(jsonpath);
 
 app.get('/allstu', (req, res) => {
-	let stulist = "";
-	Object.entries(allstu).forEach(([key, value]) => {
+	let allstu2 = loadJSON(jsonpath), stulist = "";
+	console.log("file = cache ?" + (allstu === allstu2));
+	Object.entries(allstu2).forEach(([key, value]) => {
 		stulist += `\"${key}\":\"${value}\"<br>`
 	})
 	res.send(stulist);
@@ -35,10 +50,12 @@ app.get('/searchstu', (req, res) => {
 
 app.post('/addstu', (req, res) => {
 	allstu[req.body.id] = req.body.name;
+	saveJSON(jsonpath, allstu);
 	res.send(`added ${req.body.id} : ${req.body.name}`);
 });
 
 app.post('/delstu', (req, res) => {
 	delete allstu[req.body.id];
+	saveJSON(jsonpath, allstu);
 	res.send(`deleted ${req.body.id}`);
 });
