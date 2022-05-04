9b73010bef93aae9b70936441fd53cdd97f6eadb
kc71486 <linda20030102@gmail.com>
2022-05-04 14:33:10 +0800

    test node console

--- ser.js
+++ ser.js
@@ -4,9 +4,12 @@ import express from 'express'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import bodyParser from 'body-parser'
+import { createRequire } from 'module'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const jsonpath = "students.json";
+const require = createRequire(import.meta.url);
+const { Console } = require("node:console");
+const jsonpath = "./students.json";
 
 const app = express();
 const port = 1268;
@@ -22,40 +25,66 @@ app.use(bodyParser.json());
 
 function saveJSON(src, obj) {
 	let tmp = JSON.stringify(obj), fs = require('fs');
-	fs.writeFile(src, tmp, function(err) {
+	fs.writeFile(jsonpath, tmp, function (err) {
 		if (err) {
 			console.log(err);
 		}
 	});
 }
 function loadJSON(src) {
-	let tmp = require(src);
-	return JSON.parse(tmp);
+	console.log("loaded");
+	return require(src);
 }
 
 var allstu = loadJSON(jsonpath);
 
 app.get('/allstu', (req, res) => {
-	let allstu2 = loadJSON(jsonpath), stulist = "";
-	console.log("file = cache ?" + (allstu === allstu2));
-	Object.entries(allstu2).forEach(([key, value]) => {
+	Console.log("allstu");
+	let stulist = "";
+	//var == file, so use var(and avoid file cache)
+	Object.entries(allstu).forEach(([key, value]) => {
 		stulist += `\"${key}\":\"${value}\"<br>`
 	})
 	res.send(stulist);
 });
 
 app.get('/searchstu', (req, res) => {
-	res.send("hello, " + allstu[req.query.id]);
+	let message = {};
+	if(allstu.hasOwnProperty(req.query.id)) {
+		message.mode = 0;
+		message.name = allstu[req.query.id];
+	} else {
+		message.mode = 1;
+	}
+	res.send(JSON.stringify(message));
 });
 
 app.post('/addstu', (req, res) => {
-	allstu[req.body.id] = req.body.name;
-	saveJSON(jsonpath, allstu);
-	res.send(`added ${req.body.id} : ${req.body.name}`);
+	let message = {id:req.body.id};
+	if(allstu.hasOwnProperty(req.body.id)) {
+		message.oldName = allstu[req.body.id];
+		allstu[req.body.id] = req.body.name;
+		saveJSON(jsonpath, allstu);
+		message.mode = 1;
+		message.newName = req.body.name;
+	} else {
+		allstu[req.body.id] = req.body.name;
+		saveJSON(jsonpath, allstu);
+		message.mode = 0;
+		message.newName = req.body.name;
+	}
+	res.send(JSON.stringify(message));
 });
 
 app.post('/delstu', (req, res) => {
-	delete allstu[req.body.id];
-	saveJSON(jsonpath, allstu);
-	res.send(`deleted ${req.body.id}`);
+	let message = {};
+	if(allstu.hasOwnProperty(req.body.id)) {
+		message.name = req.body.id+":"+allstu[req.body.id];
+		delete allstu[req.body.id];
+		saveJSON(jsonpath, allstu);
+		message.mode = 0;
+	} else {
+		message.mode = 1;
+	}
+	res.send(JSON.stringify(message));
 });
